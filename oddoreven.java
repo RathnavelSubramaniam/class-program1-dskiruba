a55b157006e21936fda35acf8ce209a4de13e818
dskiruba <142865936+dskiruba@users.noreply.github.com>
2023-08-22 09:04:58 +0000

    COMPLETED ODDOREVEN

---
+++ oddoreven.java
@@ -0,0 +1,9 @@
+class oddoreven{
+    public static void main(String args[]){
+        int num=3;
+        if(num%2==0){
+        System.out.println("the given number is EVEN");}
+        else{
+            System.out.println("the given number is ODD");}
+        }
+    }
